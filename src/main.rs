84b1a9a7d98971012fcdaf37803213fe24d87394
Ivy J <34287279+ivyjsgit@users.noreply.github.com>
2019-02-27 09:47:33 -0600

    Updated

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::str;
 use std::sync::{Arc, Mutex, MutexGuard};
 use std::thread;
-
+extern crate regex;
+use regex::Regex;
 fn main() -> io::Result<()> {
+    // let betweenME = "GET /hello.htm HTTP/1.1";
+    // let hmm = betweenGetHTTP(betweenME);
+    // println!("{}",hmm);
     let count = Arc::new(Mutex::new(0));
     let success = Arc::new(Mutex::new(0));
 
@@ -19,7 +23,7 @@ fn main() -> io::Result<()> {
             let success = success.clone();
             thread::spawn(move || {
                 acceptAndRespond(stream, count, success);
-//                println!("Spawned a thread!");
+            //    println!("Spawned a thread!");
             });
         }
     }
@@ -29,30 +33,37 @@ fn main() -> io::Result<()> {
 
 fn acceptAndRespond(stream: (TcpStream, SocketAddr), count: Arc<Mutex<i32>>, success: Arc<Mutex<i32>>) {
     let mut count = count.lock().unwrap();
-    *count += 1;
 
     let mut mutStream = stream.0;
     let socketAddr = stream.1;
     let getRequest = get_GET_request(&mut mutStream);
-    let requestedPath = getPathFromGET(&getRequest);
-
-    if requestedPath.contains("../") {
-        let mut success = success.lock().unwrap();
-        let returnMe: String = "HTTP/1.1 403 Forbidden\nCount:".to_string() + &count.to_string() + "\nSuccessful: " + &success.to_string() + "\n\n<html><body><h1>Error 403</h1></body></html>";
-        mutStream.write(returnMe.as_bytes());
-//        println!("Total number of requests: {}. Proper requests: {}", *count, *success);
-        return;
+    let actualRequest = betweenGetHTTP(&getRequest);
+    // println!("Actual request: {}", &actualRequest);
+    // println!("Our get request is {}", &actualRequest);
+    // let requestedPath = getPathFromGET(&getRequest);
+    let multiRequest = getMultiplePaths(&actualRequest);
+    // println!("{:?}", multiRequest);
+    for requestedPath in multiRequest{
+        // println!("Currently requested page is : {}", requestedPath);
+        if requestedPath.contains("../") {
+            let mut success = success.lock().unwrap();
+            let returnMe: String = "HTTP/1.1 403 Forbidden\nCount:".to_string() + &count.to_string() + "\nSuccessful: " + &success.to_string() + "\n\n<html><body><h1>Error 403</h1></body></html>";
+            mutStream.write(returnMe.as_bytes());
+            return;
     } else {
+        // println!("Opening {}", &requestedPath);
         let responseToSend = openFileFromPath(&requestedPath, &count, &success);
         let responseAsBytes = responseToSend.0.as_bytes();
         mutStream.write(responseAsBytes);
-        println!("{:?}",&responseToSend.1);
+        // println!("{:?}",&responseToSend.1);
         mutStream.write(&responseToSend.1);
 
 //        println!("Serving {}", requestedPath);
     }
     let mut success = success.lock().unwrap();
+        *count += 1;
     println!("Total number of requests: {}. Proper requests: {}", *count, *success);
+    }
 }
 
 fn get_GET_request(mutStream: &mut TcpStream) -> String {
@@ -60,6 +71,7 @@ fn get_GET_request(mutStream: &mut TcpStream) -> String {
     let mut buffer = [0; 500];
     mutStream.read(&mut buffer);
     let asString = str::from_utf8(&mut buffer).unwrap().to_string();
+
     return asString;
 }
 
@@ -74,7 +86,7 @@ fn getPathFromGET(getRequest: &String) -> String {
 
 fn openFileFromPath(path: &String, completed: &MutexGuard<i32>, success: &Arc<Mutex<i32>>) -> (String,Vec<u8>) {
     let mutPath = &path;
-    println!("{}", mutPath);
+    // println!("{}", mutPath);
     let mut relPath = "www".to_string() + mutPath;
     let mut file = File::open(relPath);
     match file {
@@ -85,7 +97,7 @@ fn openFileFromPath(path: &String, completed: &MutexGuard<i32>, success: &Arc<Mu
             let mut byteBuff = Vec::new();
             let mut buf: String = "HTTP/1.1 200 OK\nCount:".to_string() + &completed.to_string() + "\nSuccessful:" + &success.to_string() + "\n\n";
             let asVec = File::read_to_end(&mut mutE, &mut byteBuff);
-            println!("{:?}", byteBuff);
+            // println!("{:?}", byteBuff);
             return (buf,byteBuff);
         }
         Err(e) => {
@@ -93,4 +105,30 @@ fn openFileFromPath(path: &String, completed: &MutexGuard<i32>, success: &Arc<Mu
             return ("HTTP/1.1 404 Not Found\nCount:".to_string() + &completed.to_string() + "\nSuccessful:" + &success.to_string() + "\n\n<html><body><h1>Error 404 </h1></body></html>", Vec::new());
         }
     }
+}
+fn getMultiplePaths(getRequest: &String) -> Vec<String> {
+    // println!("{}", getRequest);
+    let mut splitted: Vec<&str> = getRequest.split("\n").collect();
+    let mut output:Vec<String> = Vec::new();
+    // splitted.remove(0);
+    // let mut index = splitted[1];
+    for mut path in splitted{
+        if (path == "/") {
+            path = "/index.html";
+        }
+        output.push(String::from(path));
+    }
+    return output;
+}
+
+fn betweenGetHTTP(line: &str) -> String{
+    let withoutGet: String = line.splitn(2, "GET").collect();
+    let mut withoutHTTP: String = withoutGet.splitn(2, "HTTP/1.1").collect();
+    withoutHTTP = withoutHTTP.trim().to_string();
+    let out:Vec<&str> = withoutHTTP.split(" ").collect();
+    return String::from(out[0]);
+    // let re = Regex::new("GET(.*?)HTTP/1.1").unwrap();
+    // let mat = re.find(line).unwrap();
+    // println!("{}", mat.start());
+    // return 
 }
